28d7879503218aadeb8154d6ed475347cf133698
Алексей Дидык <adidyk110@gmail.com>
2024-03-03 18:13:22 +0200

    Додано функцію power_of_2 та її виклик

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from utils import is_prime
 print(is_prime(7))
+from utils import power_of_2
+        print(stepin(16))
--- utils.py
+++ utils.py
@@ -10,3 +10,5 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+def stepin5(n):
+            return n != 0 and ((n & (n - 1)) == 0) and (n != 1)
